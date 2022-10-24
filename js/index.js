72aa1b3181ccc42caea747a506fd30cf7a3a228a
Oscar Mattsson <oscar.mattsson.790@gmail.com>
2022-10-24 14:28:04 -0600

    Done
    Done

---
+++ js/index.js
@@ -0,0 +1,80 @@
+let insults = [
+  {
+    insult:
+      "Were such things here as we do speak about? Or have we eaten on the insane root That takes the reason prisoner?",
+    play: "Macbeth",
+  },
+  {
+    insult: "Never hung poison on a fouler toad",
+    play: "Richard III",
+  },
+  {
+    insult: "He thinks too much: such men are dangerous.",
+    play: "Julius Ceasar",
+  },
+  {
+    insult:
+      "Thou calledst me a dog before thou hadst a cause. But since I am a dog, beware my fangs.",
+    play: "The Merchant of Venice",
+  },
+  {
+    insult: "Give me your hand...I can tell your fortune. You are a fool.",
+    play: "The Two Noble Kinsmen",
+  },
+  {
+    insult:
+      "He smells like a fish, a very ancient and fish-like smell, a kind of not-of-the-newest poor-John. A strange fish!",
+    play: "The Tempest",
+  },
+  {
+    insult:
+      "It is a tale Told by an idiot, full of sound and fury, Signifying nothing.",
+    play: "Macbeth",
+  },
+  {
+    insult:
+      "Alas, poor heart, that kiss is comfortless As frozen water to a starved snake.",
+    play: "Titus Andronicus",
+  },
+  {
+    insult:
+      "He hath eaten me out of house and home; he hath put all substance into that fat belly of his.",
+    play: "Henry IV, Part 2",
+  },
+  {
+    insult:
+      "Out, you green-sickness carrion! Out, you baggage! You tallow-face!",
+    play: "Romeo and Juliet",
+  },
+];
+
+const citat = document.querySelector("#citat");
+const name = document.querySelector("#name");
+const btn = document.querySelector("#btn");
+btn.addEventListener("click", generateInsult);
+
+let removedItem = [];
+
+function generateInsult() {
+  let index = renderIndex(insults);
+
+  if (insults.length < 1) {
+    insults = [...removedItem];
+    removedItem = [];
+  }
+  citat.innerHTML = insults[index].insult;
+  name.innerHTML = insults[index].play;
+
+  // console.log(" insults array: ", insults);
+  // console.log(" The insult ", insults[index]);
+
+  removedItem.push(insults[index]);
+  // console.log("Removed Items list:", removedItem);
+  insults.splice(index, 1);
+  // console.log(" Insults Array AFTER splice", insults[index]);
+}
+// console.log(generateInsult());
+
+window.onload = function () {
+  btn.click();
+};
